5f81abfd8c23be93aaef6c9bdef71386ff76e08b
MrSpeed30 <mr.speedman174@gmail.com>
2026-02-12 18:51:10 +0300

    Created db's and created html for about

--- locallibrary_SD/about/models.py
+++ locallibrary_SD/about/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+#review tab's data where user inputs his thoughts
+class review(models.Model):
+    email = models.EmailField(max_length=255, unique=True)
+    date = models.TimeField(auto_now=True)
+    comment = models.TextField(max_length=1500)
